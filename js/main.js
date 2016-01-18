426fb00b60c200a9100be674ddee7fe5fe0472e2
adamperlin <adamperlin@seattleacademy.org>
2016-01-18 06:35:31 -0800

    add css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-$("a").mouseenter(function() {
- $(this).animate({
-    fontSize: "1.5em"
-  }, 300 );
-  console.log("Worked");
-});
-$("a").mouseleave(function() {
- $(this).animate({
-    fontSize: "1em"
-  }, 300);
-  console.log("mouse has left");
+
+$(document).ready(()=>{
+  $(document).scroll(()=>{
+    $("myNav").css("background-color: transparent");
+    console.log("event recieved");
   });
+
+  $("a").mouseenter(function() {
+   $(this).animate({
+      fontSize: "1.5em"
+    }, 300 );
+    console.log("Worked");
+  });
+  $("a").mouseleave(function() {
+   $(this).animate({
+      fontSize: "1em"
+    }, 300);
+    console.log("mouse has left");
+    });
+  $(".navmem").click(()=>{
+    $(this).addClass("active");
+  });
+
+});
